69f876e99f323343851d32f0e97f66233f6aabef
siba98 <gyansiba.patro@gmail.com>
2021-10-10 12:22:05 +0530

    Siba:[Added] REGEX Pattern For Password Given By The User

---
+++ Day20_REGEX_Assignments/UC5_6_7_8_Password/Password_Pattern.cs
@@ -0,0 +1,22 @@
+﻿using Day20_REGEX_Assignments.CustomException;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day20_REGEX_Assignments
+{
+    public class Password_Pattern
+    {
+        //public static string Regex_PSWD = "^([$#.%+@-~!]{1}([a-zA-Z0-9]*[A-Z]{1,}[0-9]{1,})){8,}$";
+        public static string Regex_PSWD = "(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-])[a-zA-Z0-9!@#$%^&*()_+=-]{8,}$";
+        public bool ValidatePassword(string password)
+        {
+            if(password == null)
+            {
+                throw new UserRegistrationCustomException(UserRegistrationCustomException.ExceptionsType.INVALID_MESSAGE, "Invalid Password");
+            }
+            return Regex.IsMatch(password, Regex_PSWD);
+        }
+    }
+}
