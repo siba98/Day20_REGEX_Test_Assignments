81db9df01d16bd70b71806c0e46dad0d688a1e11
siba98 <gyansiba.patro@gmail.com>
2021-10-10 12:27:46 +0530

    Siba:[Added] Custom Exception Message If User Entered Something Wrong(Like Null)

---
+++ Day20_REGEX_Assignments/CustomException/UserRegistrationCustomException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day20_REGEX_Assignments.CustomException
+{
+    public class UserRegistrationCustomException : Exception
+    {
+        public enum ExceptionsType
+        {
+            INVALID_MESSAGE
+        }
+        public ExceptionsType type;
+        public UserRegistrationCustomException(ExceptionsType Type, string message) : base(message)
+        {
+            this.type = Type;
+
+        }
+    }
+}
